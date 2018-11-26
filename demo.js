3d1559310e99ada7eb0dfdc0fee45c0780c06138
KesKru <k.krugelis@gmail.com>
2018-11-27 00:38:39 +0200

    Nothing important

--- demo.js
+++ demo.js
@@ -6,19 +6,19 @@ function pow(x, n) {
 
   var result = 1;
 
-  for (  let i = 0; i < n; i++ ) {
+  for (let i = 0; i < n; i++) {
     result *= x;
   }
 
   return result;
 }
 
-function pow(x, n  ) {
-   if (n < 0) {
+function pow(x, n) {
+  if (n < 0) {
     alert("Negative 'n' not supported");
     return;
   }
-``
+  ``
   let result = 1;
 
   for (let i = 0; i < n; i++) {
