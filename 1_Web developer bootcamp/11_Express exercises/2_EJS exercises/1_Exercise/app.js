97781a67c02f73988cd760e2fc449ff2fb204b72
Kestutis Krugelis <k.krugelis@gmail.com>
2018-12-28 09:09:03 +0000

    more ejs exercises

--- 1_Web developer bootcamp/11_Express exercises/2_EJS exercises/1_Exercise/app.js
+++ 1_Web developer bootcamp/11_Express exercises/2_EJS exercises/1_Exercise/app.js
@@ -3,22 +3,27 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-let arr = 
+let list = ["list item 1", "list item 2", "list item 3"];
 /* <---------------------------------------------------> */
 
 
 app.get("/", function(req, res) {
     // res.send("Hi there !!");
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/love/:thing", function(req, res) {
     // res.send("Hi there !!");
     let thing = req.params.thing;
-    res.render("love.ejs", {thing:thing});
+    res.render("love", {thing:thing});
 });
 
+app.get("/list", function(req, res) {
+    res.render("list", {list:list});
+});
 
 
 /* <---------------------------------------------------> */
