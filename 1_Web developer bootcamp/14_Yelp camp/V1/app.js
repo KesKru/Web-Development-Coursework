8bcead3e4ac517349627632c6c554244576f66ec
Kestutis Krugelis <k.krugelis@gmail.com>
2018-12-29 11:03:53 +0000

    Added a form for new new campgrounds, some other minor changes.

--- 1_Web developer bootcamp/14_Yelp camp/V1/app.js
+++ 1_Web developer bootcamp/14_Yelp camp/V1/app.js
@@ -11,11 +11,11 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 let campgrounds = [
-    {name: "Camp-1", img: "https://farm1.staticflickr.com/7669/16417844773_f6b00d46db_o.jpg"},
-    {name: "Camp-2", img: "https://farm8.staticflickr.com/6164/6233656772_7f91862104_o.jpg"},
-    {name: "Camp-3", img: "https://farm6.staticflickr.com/5294/5433932379_6849672342_b.jpg"},
-    {name: "Camp-4", img: "https://farm1.staticflickr.com/2163/2398522822_0a29a4c096_o.jpg"},
-    {name: "Camp-5", img: "https://c4.staticflickr.com/8/7523/16103236218_a9fef9d0c2_o.jpg"},
+    { name: "Camp-1", img: "https://farm1.staticflickr.com/7669/16417844773_f6b00d46db_o.jpg" },
+    { name: "Camp-2", img: "https://farm8.staticflickr.com/6164/6233656772_7f91862104_o.jpg" },
+    { name: "Camp-3", img: "https://farm6.staticflickr.com/5294/5433932379_6849672342_b.jpg" },
+    { name: "Camp-4", img: "https://farm1.staticflickr.com/2163/2398522822_0a29a4c096_o.jpg" },
+    { name: "Camp-5", img: "https://c4.staticflickr.com/8/7523/16103236218_a9fef9d0c2_o.jpg" },
 ];
 
 /* <-------------------------Routes--------------------------> */
@@ -25,9 +25,20 @@ app.get("/", function (req, res) {
 });
 
 app.get("/campgrounds", function (req, res) {
-    res.render("campgrounds", {campgrounds:campgrounds});
+    res.render("campgrounds", { campgrounds: campgrounds });
 });
 
+app.get("/campgrounds/new", function (req, res) {
+    res.render("newCampground");
+});
+
+app.post("/campgrounds", function (req, res) {
+    let campgroundName = req.body.campgroundName;
+    let url = req.body.url;
+    let newCampground = { name: campgroundName, img: url };
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+});
 
 
 
