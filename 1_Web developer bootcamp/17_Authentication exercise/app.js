49f0a5bca7282e49d4cf348a57479fd240a20666
Kestutis Krugelis <k.krugelis@gmail.com>
2019-01-05 13:49:11 +0000

    Authentication exercise update

--- 1_Web developer bootcamp/17_Authentication exercise/app.js
+++ 1_Web developer bootcamp/17_Authentication exercise/app.js
@@ -14,20 +14,34 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(expressSession({ 
-    secret: "this text is used to decode and encode the sessions", 
-    resave: false, 
+app.use(expressSession({
+    secret: "this text is used to decode and encode the sessions",
+    resave: false,
     saveUninitialized: false
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+passport.use(new passportLocal(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-mongoose.set('useCreateIndex', true);
-mongoose.connect("mongodb://localhost/auth_demo", { useNewUrlParser: true, useFindAndModify: false });
+mongoose.connect("mongodb://localhost/auth_demo", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
 
+/* <-----------------------------------------------------------> */
+
+// MIDLEWARE to prevent  acces to secret by url
+function isLogedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        res.redirect("/login");
+    }
+}
 /* <-----------------------------------------------------------> */
 
 // Home 
@@ -36,133 +50,58 @@ app.get("/", function (req, res) {
 });
 
 // Secret 
-app.get("/secret", function (req, res) {
+app.get("/secret", isLogedIn, function (req, res) {
     res.render("secret");
 });
 
+/* <-----------------------------------------------------------> */
+
+// REGISTER FORM
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+// REGISTER
+app.post("/register", function (req, res) {
+    User.register(new User({ username: req.body.username }), req.body.password,
+        function (err, user) {
+            if (err) {
+                console.log(err);
+            } else {
+                passport.authenticate("local")(req, res, function () {
+                    res.redirect("/secret");
+                    console.log(user);
+                });
+            }
+        });
+});
+
+//--------------------------------------
+
+// LOGIN FORM
+app.get("/login", function (req, res) {
+    res.render("login");
+});
 
 
+// LOGIN 
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function (req, res) {
+});
 
+//--------------------------------------
 
+// LOGOUT 
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-/* <-------------------------Routes--------------------------> */
-
-// RESTful routes template
-
-// INDEX   |  /dogs          |  GET     | List all dogs.
-//--------------------------------------------------------------------------------
-// NEW     |  /dogs/new      |  GET     | Show new dog form.
-// CREATE  |  /dogs          |  POST    | Create new dog, then redirect somewhere.
-//--------------------------------------------------------------------------------
-// SHOW    |  /dogs/:id      |  GET     | Show info about one specific dog.
-//--------------------------------------------------------------------------------
-// EDIT    |  /dogs/:id/edit |  GET     | Show edit form for a dog.
-// UPDATE  |  /dogs/:id      |  PUT     | Update specific dog, then redirect somewhere.
-//--------------------------------------------------------------------------------
-// DESTROY |  /dogs/:id      |  DELETE  | Delete specific dog, then redirect somewhere.
-
-//--------------------------------------------------------------------------------
-
-// INDEX | /dogs | GET | List all dogs.
-// app.get("/posts", function (req, res) {
-//     Post.find({},
-//         function (err, allPosts) {
-//             if (err) {
-//                 console.log(err);
-//             } else {
-//                 res.render("posts-index", { allPosts: allPosts });
-//             };
-//         });
-// });
-
-//--------------------------------------------------------------------------------
-
-// NEW | /dogs/new | GET | Show new dog form.
-// app.get("/posts/new", function (req, res) {
-//     res.render("posts-new");
-// });
-
-// CREATE | /dogs | POST | Create new dog, then redirect somewhere.
-// app.post("/posts", function (req, res) {
-//     Post.create(req.body.blogPost,
-//         function (err, post) {
-//             if (err) {
-//                 console.log(err);
-//             } else {
-//                 console.log("New post reated !!");
-//             }
-//         });
-//     res.redirect("/posts");
-// });
-
-//--------------------------------------------------------------------------------
-
-// SHOW | /dogs/:id | GET | Show info about one specific dog.
-// app.get("/posts/:id", function (req, res) {
-//     Post.findById(req.params.id,
-//         function (err, postById) {
-//             if (err) {
-//                 console.log(err);
-//             } else {
-//                 res.render("posts-show", { postById: postById });
-//             };
-//         });
-// });
-
-//--------------------------------------------------------------------------------
-
-// EDIT | /dogs/:id/edit | GET | Show edit form for a dog.
-// app.get("/posts/:id/edit", function (req, res) {
-//     Post.findById(req.params.id,
-//         function (err, postById) {
-//             if (err) {
-//                 console.log(err);
-//             } else {
-//                 res.render("posts-edit", { postById: postById });
-//             };
-//         });
-// });
-
-// UPDATE | /dogs/:id | PUT | Update specific dog, then redirect somewhere.
-// app.put("/posts/:id", function (req, res) {
-//     Post.findByIdAndUpdate(req.params.id, req.body.blogPost,
-//         function (err, updatedBlog) {
-//             if (err) {
-//                 console.log(err);
-//             } else {
-//                 res.redirect("/posts/" + req.params.id);
-//             };
-//         });
-// });
-
-//--------------------------------------------------------------------------------
-
-// DESTROY | /dogs/:id | DELETE | Delete specific dog, then redirect somewhere.
-// app.delete("/posts/:id", function (req, res) {
-//     Post.findByIdAndRemove(req.params.id,
-//         function (err) {
-//             if (err) {
-//                 console.log(err);
-//             } else {
-//                 res.redirect("/posts");
-//             };
-//         });
-// });
-
-/* <---------------------------------------------------> */
+/* <-----------------------------------------------------------> */
 
 app.listen(3000, function () {
     console.log("Server has started !");
 });
+
--- 1_Web developer bootcamp/17_Authentication exercise/models/user.js
+++ 1_Web developer bootcamp/17_Authentication exercise/models/user.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 
-let userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     author: String,
     image: String,
     body: String,
-    date: { type: Date, default: Date.now }
-    //incoming form fileds should be named acordingly and grouped in order to just pass in the object when creating or editing.
+    date: { type: Date, default: Date.now },
+    // incoming form fileds should be named acordingly and grouped in order to just pass in the object when creating or editing.
 });
 userSchema.plugin(passportLocalMongoose);
 
