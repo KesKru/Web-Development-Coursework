56820632cb42db71d3bbb0c265ff498f655cc526
Kestutis Krugelis <k.krugelis@gmail.com>
2019-03-19 23:10:46 +0200

    bootstrap experimets

--- Random-stuff/Neagoie-tutorials/smart-brain/src/components/Navigation/Navigation.js
+++ Random-stuff/Neagoie-tutorials/smart-brain/src/components/Navigation/Navigation.js
@@ -3,13 +3,13 @@ import React from 'react';
 export default function Navigation() {
   return (
     <nav className="nav justify-content-end">
-      <a className="nav-link active" href="#">
+      <a className="nav-link active" href="/">
         Active
       </a>
-      <a className="nav-link" href="#">
+      <a className="nav-link" href="/">
         Link
       </a>
-      <a className="nav-link" href="#">
+      <a className="nav-link" href="/">
         Link
       </a>
     </nav>
