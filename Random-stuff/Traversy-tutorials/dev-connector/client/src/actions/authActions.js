df039ec0896035dc253ea1d897b7a3ac7b5fa9b1
Kestutis Krugelis <k.krugelis@gmail.com>
2019-04-27 18:55:26 +0300

    Finished dev-connector example, reorganized some folders

--- Random-stuff/Traversy-tutorials/dev-connector/client/src/actions/authActions.js
+++
@@ -1,9 +0,0 @@
-import { TEST123 } from './types';
-// TEST
-
-export const test123 = (userData) => {
-  return {
-    type: TEST123,
-    payload: userData
-  };
-};
--- Random-stuff/Traversy-tutorials/dev-connector/server/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  dbUrl: 'mongodb://localhost/dev-connector',
-  secret: 'secret123'
-};
